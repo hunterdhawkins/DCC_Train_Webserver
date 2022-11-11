2154c2ed43f34f471d4d8b9d79a7bfc2f7141b07
Hunter <hunterdhawkins@gmail.com>
2022-11-11 11:22:08 -0800

    Train Model

--- train_project/home/models.py
+++ train_project/home/models.py
@@ -5,3 +5,17 @@ class Success_Log(models.Model):
     was_successful = models.BooleanField()
     time_taken = models.DateTimeField()
     reason_for_failure = models.TextField(default="None")
+
+
+class Train_Data(models.Model):
+    train_location = models.IntegerField()
+    time_taken = models.DateTimeField()
+    train_speed = models.IntegerField()
+
+
+# train_location
+# timestamp
+# speed
+
+# sensor
+# sensor data
