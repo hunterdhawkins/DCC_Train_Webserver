a0ea7bfaea6e8a21ea2a5b4e446fbff829b79309
Hunter <hunterdhawkins@gmail.com>
2022-11-16 14:11:46 -0800

    Can read and write modbus coils

--- modbus_utils.py
+++ modbus_utils.py
@@ -1,3 +1,12 @@
+import pymodbus
+from pymodbus.client.sync import ModbusTcpClient
+'''
+    PyModBus Sources:
+    https://blog.jonasneubert.com/2019/11/02/using-pymodbus-to-communicate-with-a-plc/
+    Functions( https://pymodbus.readthedocs.io/en/latest/source/library/pymodbus.client.html)
+'''
+
+
 def read_single_modbus_coil(address):
     result = client.read_coils(1, 1)
     print(result.bits[0])
@@ -6,17 +15,26 @@ def read_single_modbus_coil(address):
 
 def modbus_quick_check():
     try:
-        client = ModbusTcpClient('127.0.0.1')
+        client = ModbusTcpClient('129.101.98.229')
 
         # Param 1: address – Start address to read from
         # Param 2: value – Boolean to write
-        # client.write_coil(1, True)
+        # client.write_coil(400, True)
 
         # Param 1: address – Start address to read from
         # Param 2: count – (optional) Number of coils to read
-        result = client.read_coils(1, 1)
-
+        result = client.read_coils(400, 100)
+        print(result.bits)
         print(result.bits[0])
+
         client.close()
     except:
         print("An exception occurred") 
+
+
+def main():
+    modbus_quick_check()
+
+
+if __name__ == '__main__':
+    main()
